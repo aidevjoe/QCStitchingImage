15d149af1b3ca9fdd9d380a86d0e7c380bb3ef9b
Joe <joesir7@foxmail.com>
2016-08-11 16:04:32 +0800

    图片

--- QCStitchingImage/ViewController.m
+++ QCStitchingImage/ViewController.m
@@ -17,7 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-        NSArray * arr = @[
+        NSArray * networkArray = @[
                          @"http://wx.qlogo.cn/mmopen/SEQ2uumQjyuWXqk0HmBcJOUTyvg4WwRBkg3X3h2U3ksZvCc9wPVz7wQ5WQlUb2v2PhnVyUAPcQXibMZyMDc1HzQ/0",
                          @"http://wx.qlogo.cn/mmopen/9C2tmO90VybnuU61jtDN9PSBu5PeqncYlS5Bkxv7epCzmws0SmYqDBDzLVkoH5hC168dBcVNfIaZ4tcqkwG6hP61ciavwhy1a/0",
                          @"http://wx.qlogo.cn/mmopen/ibRqXwrJbwScwiaWmlPEnnuS4BjFkPB81oMLH0I3M9BVe16R6icE0PBY0OuqBGl9yKHn1NJbdqPnfjuPv65LEGd0ChH6BicURrCw/0",
@@ -28,11 +28,14 @@
                          @"http://wx.qlogo.cn/mmopen/fiaTE6Cian9ibdmGcOXmg8gZDtd6J42rB2OpkNjlX7BNBHCPePJfcaUhicTm2ibm84CNO67tVIKltjImGaqvXyH9CzcVhvUw7HELA/0",
                          @"http://wx.qlogo.cn/mmopen/ajNVdqHZLLAJYLkTTAFVXQrcbxNTPBz8yicBw7oDQtoRyFyQvqc3Mo9k2zX8U8nB9KkNLicIBPzenSEER5jP3azA/0"];
 
-//    NSArray *arr = @[@"1", @"2", @"3", @"4", @"5"];
-    QCStitchingImage *stitchingImage = [QCStitchingImage stitchingWithImageArray:arr];
-    stitchingImage.center = self.view.center;
+    
+    QCStitchingImage *stitchingImage = [QCStitchingImage stitchingWithFrame:CGRectMake(50, 50, 150, 150) imageArray: networkArray];
     [self.view addSubview:stitchingImage];
     
+    
+    NSArray *locationArray = @[@"1", @"2", @"3", @"4", @"5", @"6", @"7"];
+    QCStitchingImage *stitchingImage2 = [QCStitchingImage stitchingWithFrame:CGRectMake(50, 210, 150, 150) imageArray:locationArray];
+    [self.view addSubview:stitchingImage2];
 }
 
 
